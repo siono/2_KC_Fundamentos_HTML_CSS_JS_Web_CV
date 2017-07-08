33627f3066d212bf7a0467e960fada4923c4a0ed
Jose Carlos Esteban <jc.esteban.gomez@gmail.com>
2017-07-08 21:40:01 +0200

    Added smooth scroll and other changes

--- js/menu.js
+++ js/menu.js
@@ -4,10 +4,11 @@ for (i=0; i< navbarItems.length; i++){
     navbarItems[i].addEventListener("click", function (event){
         
         var sectionToGo = this.getElementsByTagName("a")[0].href.split("#");
-        console.log(sectionToGo);
+        
         if (sectionToGo.length === 2){
-            event.preventDefault();
+            event.preventDefault();//detengo la opción por defecto de <a>
             var goTo = sectionToGo[sectionToGo.length - 1];
+            
             getElementByIdAndScroll(goTo);
         }
 
@@ -27,7 +28,8 @@ function getElementByIdAndScroll (id) {
 }
 
 function scrollToElement(element){
-    var jump = element.getBoundingClientRect().top * 0.3; //calcula la distancia de salto relativa al elemento.
+    debugger;
+    var jump = (element.getBoundingClientRect().top - 60 ) * 0.3; //calcula la distancia de salto relativa al elemento.
     if ( !element.lastJump || element.lastJump > Math.abs(jump)) {
         element.lastJump = Math.abs(jump);
         setTimeout(function(){
@@ -36,7 +38,8 @@ function scrollToElement(element){
     }else{
         element.lastJump = null;
     }
-    document.body.scrollTop += jump;
+    document.body.scrollTop += jump; //other browser
+    document.documentElement.scrollTop +=jump; //to work only in IE
 }
 
 
